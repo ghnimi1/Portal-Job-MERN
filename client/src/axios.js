b81725458f7c277bcc7b4765c059ef96ac1301fc
ghnimihassen <ghnimih@gmail.com>
2022-10-13 19:58:33 +0100

    make it better

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4800/api'
+    baseURL: 'https://portajob.herokuapp.com/api'
 })
 
 axios.interceptors.request.use(config => {
